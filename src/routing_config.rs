3278a86c39ab39daf1d034f030f6de84eea0be7d
kshiroma <shiroma@cam-net.co.jp>
2021-08-16 13:42:27 +0900

    routing config for cam

--- src/routing_config.rs
+++ src/routing_config.rs
@@ -20,12 +20,8 @@ fn set_routing_number(config: &ServerConfig, request: &HttpRequestInfo) -> Optio
 }
 
 fn routing(config: &ServerConfig, request: &HttpRequestInfo) -> Option<RelayConnectionInfo> {
-    const HOST_A: &str = "localhost";
-    const PORT_A:i32 = 8081;
-
-    const HOST_B: &str = "localhost";
-    const PORT_B:i32 = 8080;
-
+    const jt0100: &str = "dev-jt0100";
+    const jt0001: &str = "dev-jt0001";
 
     let path: &str = if request.http_first_line.uri.eq_ignore_ascii_case("/favicon.ico") {
         "/cattleya/favicon.ico"
@@ -40,30 +36,30 @@ fn routing(config: &ServerConfig, request: &HttpRequestInfo) -> Option<RelayConn
         match n {
             1 =>
                 Some(RelayConnectionInfo::new2(
-                    HOST_A,
-                    PORT_A,
+                    jt0001,
+                    8000,
                     path,
                     "1__",
                 )),
             2 =>
                 Some(RelayConnectionInfo::new2(
-                    HOST_B,
-                    PORT_B,
+                    jt0100,
+                    8000,
                     path,
                     "2__",
                 )),
             _ =>
                 if i % 2 == 0 {
                     Some(RelayConnectionInfo::new2(
-                        HOST_A,
-                        PORT_A,
+                        jt0001,
+                        8000,
                         path,
                         "0_0",
                     ))
                 } else {
                     Some(RelayConnectionInfo::new2(
-                        HOST_B,
-                        PORT_B,
+                        jt0100,
+                        8000,
                         path,
                         "0_1")
                     )
@@ -74,3 +70,95 @@ fn routing(config: &ServerConfig, request: &HttpRequestInfo) -> Option<RelayConn
     };
     return relay;
 }
+
+fn routing_milliondollar(request: &HttpRequestInfo) -> Option<RelayConnectionInfo> {
+    let prefix: &str = "million-dollar";
+
+    let host = &request.http_request_header.host;
+    let pattern = prefix.to_string() + ".";
+    let pattern = pattern.as_str();
+    let conjunction: &str = &request.http_first_line.uri;
+    let relay = if host.starts_with(pattern) {
+        Some(RelayConnectionInfo::new2(
+            "localhost",
+            1234,
+            conjunction,
+            "million_dollar",
+        ))
+    } else {
+        None
+    };
+    return relay;
+}
+
+
+fn routing_timer(request: &HttpRequestInfo) -> Option<RelayConnectionInfo> {
+    let prefix: &str = "timer";
+    let path = "/cattleya";
+    let host = &request.http_request_header.host;
+    let pattern = prefix.to_string() + ".";
+    let pattern = pattern.as_str();
+    let conjunction: &str = if request.http_first_line.uri.contains('?') { "&" } else { "?" };
+    let relay = if host.starts_with(pattern) {
+        Some(RelayConnectionInfo::new2(
+            "dev-timer",
+            8000,
+            (path.to_string() + conjunction + "targetUser=" + prefix).as_str(),
+            "chronotrigger",
+        ))
+    } else {
+        None
+    };
+    return relay;
+}
+
+
+#[test]
+fn test() {
+    use std::io::Read;
+    use std::io::Write;
+    let relay =
+        RelayConnectionInfo::new1(
+            "localhost",
+            8080,
+            "/cattleya/view/login?targetUser=wakuden");
+    println!("relay host is {}", relay.get_address());
+
+    let mut stream = &relay.connect_relay().unwrap();
+    stream.write(b"GET ").unwrap();
+    stream.write(&relay.path.into_bytes()).unwrap();
+    stream.write(b" HTTP/1.1\r\n").unwrap();
+
+    stream.write(b"Host: localhost:8000\r\n").unwrap();
+    stream.write(b"User-Agent: curl/7.55.1\r\n").unwrap();
+    stream.write(b"Accept: */*\r\n\r\n").unwrap();
+    stream.flush().unwrap();
+    let mut data = [0; 4096 * 4];
+    stream.read(&mut data).unwrap();
+    println!("{}", String::from_utf8_lossy(&data));
+}
+
+
+#[test]
+fn test_get_address() {
+    let relay = RelayConnectionInfo::new1(
+        "localhost",
+        8080,
+        "/cattleya/view/login?targetUser=wakuden");
+    assert_eq!("localhost:8080", relay.get_address());
+    let relay = RelayConnectionInfo::new1(
+        "localhost",
+        80,
+        "/cattleya/view/login?targetUser=wakuden");
+    assert_eq!("localhost", relay.get_address());
+    let relay = RelayConnectionInfo::new1(
+        "localhost",
+        0,
+        "/cattleya/view/login?targetUser=wakuden",
+    );
+    assert_eq!("localhost", relay.get_address());
+}
+
+
+#[test]
+fn test_get_user_name() {}
